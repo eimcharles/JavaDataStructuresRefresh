7ae5dcaa7d82233b81f510de77955572796f753a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-22 10:31:16 -0500

    Example one: User example manipulating arrays, added question 7 for getUserById() operation.

--- src/dataStructures/arrays/exerciseOne/user/UserExample.java
+++ src/dataStructures/arrays/exerciseOne/user/UserExample.java
@@ -2,6 +2,7 @@ package dataStructures.arrays.exerciseOne.user;
 
 import dataStructures.arrays.exerciseOne.user.dao.ArrayUserDAO;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 public class UserExample {
@@ -12,7 +13,7 @@ public class UserExample {
 
         ///  Question 1
         System.out.println("Question 1: get all users");
-        arrayUserDAO.displayAllUsers();
+        System.out.println(Arrays.toString(arrayUserDAO.getUsers()));
         System.out.println();
 
         ///  Question 2
@@ -31,6 +32,7 @@ public class UserExample {
 
         ///  Question 4
         System.out.println("Question 4: add user");
+
         User charles = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"),
                 "Charles",
                 "Eimer");
@@ -58,7 +60,6 @@ public class UserExample {
 
         ///  Question 6
         System.out.println("Question 6: update user");
-
         User jerryToUpdateByID = arrayUserDAO.getUserById(Jerry.getUserId());
         System.out.println("Prior id to update: " + jerryToUpdateByID.getName() + " "  + jerryToUpdateByID.getLastName());
 
@@ -67,6 +68,12 @@ public class UserExample {
 
         System.out.println("After id updated: "  + jerryToUpdateByID.getName()  + " "  + jerryToUpdateByID.getLastName());
         arrayUserDAO.displayAllUsers();
+        System.out.println();
+
+        ///  Question 7
+        System.out.println("Question 7: get user by id");
+        User jerryById = arrayUserDAO.getUserById(jerryToUpdateByID.getUserId());
+        System.out.println(jerryById);
         System.out.println();
 
     }
