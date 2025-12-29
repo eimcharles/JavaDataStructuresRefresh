c83cdbf4175b58cb4bbf417b2331c9d686d1b562
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-29 16:50:10 -0500

    Example two: implementing upper bounded wildcards for numeric subtype.

---
+++ src/generics/exerciseTwo/ExampleTwo.java
@@ -0,0 +1,95 @@
+package generics.exerciseTwo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExampleTwo {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4);
+        List<Double> doubleList = Arrays.asList(1.1, 2.2, 3.3);
+        List<Number> numberList = Arrays.asList(1.1, 2, 3, 4.4);
+
+        ///  Example 1
+        System.out.println("Example 1: printing Integers using upper bounded wildcards");
+        printNumbers(intList);
+        System.out.println();
+
+        ///  Example 2
+        System.out.println("Example 2: printing Doubles using upper bounded wildcards");
+        printNumbers(doubleList);
+        System.out.println();
+
+        ///  Example 3
+        System.out.println("Example 3: printing Numbers using upper bounded wildcards");
+        printNumbers(numberList);
+        System.out.println();
+
+        ///  Example 4
+        System.out.println("Example 4: sum of Integers using upper bounded wildcards");
+        System.out.println("Total: " + sumOfNumberList(intList));
+        System.out.println();
+
+        ///  Example 5
+        System.out.println("Example 5: sum of Doubles using upper bounded wildcards");
+        System.out.println("Total: " + sumOfNumberList(doubleList));
+        System.out.println();
+
+        ///  Example 6
+        System.out.println("Example 6: sum of Number using upper bounded wildcards");
+        System.out.println("Total: " + sumOfNumberList(numberList));
+        System.out.println();
+
+    }
+
+    /**
+     *      Wildcards (?): Upper bounds (ceiling)
+     *
+     *      Restriction: The caller can only pass a List of 'Type' Number
+     *                   or one of its subclasses.
+     *
+     *      Wildcards can ONLY have ONE bound: List<? extends Number>
+     *
+     *      PECS: Producer Extends | Consumer Super
+     *
+     *      The passed List<? extends Number> list is a Producer:
+     *
+     *      - Data Flow: The data is moving OUT of the list and INTO the method.
+     *
+     *      - Supply: The list "supplies" (produces) the Numbers required for
+     *      the sum or the print operation.
+     *
+     *      - Read-Only: Because the list is a Producer, the method acts as the
+     *      receiver.
+     *
+     * */
+
+    static void printNumbers(List<? extends Number> list) {
+
+        list.forEach( e -> {
+
+            System.out.println(e.getClass().getSimpleName() + "\t\t" + e);
+
+        });
+
+    }
+
+    static double sumOfNumberList(List<? extends Number> list) {
+
+        double sum = 0.0;
+
+        for (Number n : list) {
+
+            /**
+             *      We can call .doubleValue() because we guaranteed
+             *      the list contains Numbers or its subclasses.
+             * */
+
+            sum += n.doubleValue();
+        }
+
+        return sum;
+    }
+
+}
