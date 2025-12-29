8a54e9ed049e5230cb0e419813ed1e05cb227de8
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-29 17:28:38 -0500

    Comment on bounded type parameters T and wildcards(?).

---
+++ src/generics/BoundedVsWildcards/Example.java
@@ -0,0 +1,23 @@
+package generics.BoundedVsWildcards;
+
+public class Example {
+
+    /**
+     *          Bounded Type Parameters T VS Wildcards(?)
+     *
+     *          1. Bounded Type Parameters T:
+     *
+     *         - References to the type inside the method body.
+     *         - Relates the return type to a parameter.
+     *         - Can implement multiple Classes or Interfaces at once using & (Intersection).
+     *         - Type parameters T do NOT support lower bounds.
+     *
+     *          2. Wildcards (?):
+     *
+     *         - Wildcards(?) can ONLY have ONE bounds.*
+     *         - Wildcards(?) support upper bounds (ceiling).
+     *         - Wildcards(?) support lower bounds (floor).
+     *
+     * */
+
+}
